6c72c248210960e9c808e1ca3609381a7a9d51d1
davidaodejobi <talk2engineerdavid@gmail.com>
2021-11-09 18:23:13 +0100

    added a validation to check the inputted data when submitting the data

--- expenses_tracker/lib/widgets/new_transaction.dart
+++ expenses_tracker/lib/widgets/new_transaction.dart
@@ -11,6 +11,22 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+      // return stops the function excecution so the code below won't be
+      //reached if the condition above it is true and called(In the case of this app, transactions won't be added)
+    }
+
+    addNewTransaction!(
+      titleController.text,
+      double.parse(amountController.text),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -23,19 +39,19 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: const InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData(),
             ),
             TextField(
               decoration: const InputDecoration(labelText: 'Amount'),
               controller: amountController,
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
+              //what is done is kinda if a rule to tell flutter that we got an argument
+              //here but i don't really care about it or using it. In those cases it's adviceable to use this method to solve those issues
             ),
             TextButton(
               child: const Text('Add Transaction'),
-              onPressed: () {
-                addNewTransaction!(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: submitData,
             ),
           ],
         ),
