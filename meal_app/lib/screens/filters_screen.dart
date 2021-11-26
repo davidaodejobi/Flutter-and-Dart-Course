ea9490bad1d5ea1068d225661144b25e7723ab09
davidaodejobi <talk2engineerdavid@gmail.com>
2021-11-26 11:57:43 +0100

    added switch list tile to filter the meals

--- meal_app/lib/screens/filters_screen.dart
+++ meal_app/lib/screens/filters_screen.dart
@@ -2,11 +2,20 @@ import 'package:flutter/material.dart';
 
 import '/widgets/main_drawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   const FiltersScreen({Key? key}) : super(key: key);
 
   static const routeName = '/filters';
 
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +23,60 @@ class FiltersScreen extends StatelessWidget {
         title: const Text('Filters'),
       ),
       drawer: const MainDrawer(),
-      body: const Center(
-        child: Text('Filters\nStill working on this part'),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                buildSwitchListTile(
+                    'Gluten-free', 'Onlu include gluten-free meal',
+                    currentValue: _glutenFree, updateValue: (value) {
+                  setState(() {
+                    _glutenFree = value;
+                  });
+                }),
+                buildSwitchListTile(
+                    'Lactose-free', 'Onlu include lactose-free meal',
+                    currentValue: _lactoseFree, updateValue: (value) {
+                  setState(() {
+                    _lactoseFree = value;
+                  });
+                }),
+                buildSwitchListTile(
+                    'Vegeterian', 'Onlu include vegeterian meal',
+                    currentValue: _vegetarian, updateValue: (value) {
+                  setState(() {
+                    _vegetarian = value;
+                  });
+                }),
+                buildSwitchListTile('Vegan', 'Onlu include vegan meal',
+                    currentValue: _vegan, updateValue: (value) {
+                  setState(() {
+                    _vegan = value;
+                  });
+                }),
+              ],
+            ),
+          ),
+        ],
       ),
+    );
+  }
+
+  SwitchListTile buildSwitchListTile(String title, String description,
+      {bool? currentValue, ValueChanged<bool>? updateValue}) {
+    return SwitchListTile(
+      title: Text(title),
+      value: currentValue!,
+      subtitle: Text(description),
+      onChanged: updateValue,
     );
   }
 }
