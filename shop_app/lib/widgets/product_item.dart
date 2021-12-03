b32b30558ad67916353ea214070d3d22e2d79de9
davidaodejobi <talk2engineerdavid@gmail.com>
2021-12-03 16:20:44 +0100

    added borderRadius with ClipRRect. with some mods => theme.

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -14,27 +14,30 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Image.network(
-        imageUrl!,
-        fit: BoxFit.cover,
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black87,
-        leading: IconButton(
-          icon: const Icon(Icons.favorite),
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {},
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imageUrl!,
+          fit: BoxFit.cover,
         ),
-        title: Text(
-          title!,
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.button,
-        ),
-        trailing: IconButton(
-          icon: const Icon(Icons.shopping_cart),
-          color: Theme.of(context).colorScheme.secondary,
-          onPressed: () {},
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
+          title: Text(
+            title!,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.button,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {},
+          ),
         ),
       ),
     );
