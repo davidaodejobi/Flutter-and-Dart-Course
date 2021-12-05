4744e6d0e3a88104e6cbb5cb780a4ca4c4120a07
davidaodejobi <talk2engineerdavid@gmail.com>
2021-12-05 23:21:00 +0100

    used nested models and providers

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import '/providers/product.dart';
+import '/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String? id;
-  final String? title;
-  final String? imageUrl;
+  // final String? id;
+  // final String? title;
+  // final String? imageUrl;
 
   const ProductItem({
-    this.id,
-    this.title,
-    this.imageUrl,
+    // this.id,
+    // this.title,
+    // this.imageUrl,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -22,11 +26,11 @@ class ProductItem extends StatelessWidget {
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetailScreen.routeName,
-              arguments: id,
+              arguments: product.id,
             );
           },
           child: Image.network(
-            imageUrl!,
+            product.imageUrl!,
             fit: BoxFit.cover,
           ),
         ),
@@ -38,7 +42,7 @@ class ProductItem extends StatelessWidget {
             onPressed: () {},
           ),
           title: Text(
-            title!,
+            product.title!,
             textAlign: TextAlign.center,
             style: Theme.of(context).textTheme.button,
           ),
